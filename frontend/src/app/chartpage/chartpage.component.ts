17ebfa55cbabccf9de0109ce38e07472deede856
jeremieBitancor <bitancor.jeremie@gmail.com>
2019-01-20 19:18:11 +0800

    added homepage

---
+++ frontend/src/app/chartpage/chartpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chartpage',
+  templateUrl: './chartpage.component.html',
+  styleUrls: ['./chartpage.component.scss']
+})
+export class ChartpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
