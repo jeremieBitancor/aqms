4895bb619adf909a0281f981cd4343a673e25e2c
jeremieBitancor <bitancor.jeremie@gmail.com>
2019-02-10 18:05:53 +0800

    added average on power

--- backend/power/urls.py
+++ backend/power/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import PowerListView
+from .views import PowerListView, PowerAveListView
 
 urlpatterns = [
-    path('power/', PowerListView.as_view(), name="power-all")
+    path('power/', PowerListView.as_view(), name="power-all"),
+    path('power/average', PowerAveListView.as_view(), name="power-ave")
 ]
--- backend/power/views.py
+++ backend/power/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from power.models import Power
-from .serializers import PowerSerializer
+from .serializers import PowerSerializer, AvePowerSerializer
 from rest_framework import generics
 # Create your views here.
+from django.db.models.functions import ExtractHour, ExtractWeek
+from django.db.models import Avg
 
 
 class PowerListView(generics.ListCreateAPIView):
     queryset = Power.objects.all()
     serializer_class = PowerSerializer
+
+
+class PowerAveListView(generics.ListAPIView):
+    queryset = Power.objects.values(hour=ExtractHour(
+        'time')).annotate(ave_watts=Avg('watts'), ave_voltage=Avg('voltage'), ave_ampere=Avg('ampere')).order_by(
+            'date')
+    serializer_class = AvePowerSerializer
